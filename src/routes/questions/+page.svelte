141e3ded0c7c7527b73577c0520612d9f223a018
Anders <dom.galbavy@gmail.com>
2024-01-01 20:33:57 +0100

    added questions route

---
+++ src/routes/questions/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+
+	import Card from "$lib/components/Card.svelte"
+
+</script>
+
+<div class='flex flex-row gap-2 h-full'>
+
+	<Card>
+		Topics	
+	</Card>
+	<Card>
+		Questions
+	</Card>
+</div>
