236db643bfe3fcf38d51d8056327fb1f0c22d89b
Anders <dom.galbavy@gmail.com>
2024-01-01 18:39:51 +0100

    formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,7 +46,8 @@
 	}
 	.img-bg {
 		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
-		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
+		animation:
+			pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
 			glow 5s linear infinite;
 	}
 	@keyframes glow {
